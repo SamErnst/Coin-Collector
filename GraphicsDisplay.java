c42964b80ab143dd8302a4f9028d0f6836fc04ef
SamErnst <75223316+SamErnst@users.noreply.github.com>
2020-11-29 15:59:14 -0500

    Add files via upload

---
+++ GraphicsDisplay.java
@@ -0,0 +1,27 @@
+package coincollector;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+/**
+ *
+ * @author samue
+ */
+public class GraphicsDisplay{
+    public void displayGraphics(int timeAmt){
+        int timeRemaining = timeAmt;
+        Canvas gameMap = new Canvas();
+        Graphics gr;
+        while(timeRemaining>0){
+            int timer = timeAmt;
+            timeRemaining--;
+            
+        }
+    }
+    
+}
